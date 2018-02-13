b101b08e8ae4d852de1e590dcc9dc496f7581267
TajAhmed <go4taj@gmail.com>
2018-02-13 22:07:21 +0530

    move import-export.js to src directory

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var import_export_mixin = require("./import-export");
+var import_export_mixin = require("./src/import-export");
 
 module.exports =  function(app){
   app.loopback.modelBuilder.mixins.define('ImportExport', import_export_mixin);
